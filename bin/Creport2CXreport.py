f0dc2ed782528bcface030f367821e9d77e37d5b
vbrendel <vbrendel@indiana.edu>
2016-01-01 14:58:23 -0500

    BWASP makefiles and scripts

---
+++ bin/Creport2CXreport.py
@@ -0,0 +1,101 @@
+#
+#BWASP python script Creport2CXreport.py
+#
+# This script takes a sorted creport file (output from Bismark script coverage2cytosine)
+# and splits it into the *.CpGreport, *.CHGreport, and *.CHHreport files.  It also
+# produces the *.HSMthresholds file, which records the BWASP rationale for determining
+# the minimal coverage of a site to be dectectable as a "highly supported methylation"
+# (hsm) site.
+#
+import decimal
+import math
+import re
+import scipy.stats
+import sys
+
+def minNval (coverage, label):
+  unique_n = []
+  unique_n = list(set(coverage))
+  unique_n = map(int,unique_n)
+  unique_n.sort()
+
+  print '\n\nFinding the minimal coverage required to potentially observe significant failed C->T conversion counts ...\n'
+
+  for n in range (0,len(unique_n)):
+      breakFlag = 0
+      S_n = t = 0
+      for m in range (0,len(coverage)):
+          if int(coverage[m]) >= unique_n[n]:
+             S_n += 1
+      t = 0.01/S_n
+      for m in range(1,int(unique_n[n]+1)):
+          if 1-scipy.stats.binom.cdf(m-1,int(unique_n[n]),0.005) <= t :
+             breakFlag = 1
+             print 'For coverage n=' + str(unique_n[n]) + ', there are', S_n, label, 'sites.'
+             print 'The Bonferroni adjusted 1% significance probability threshold is t = 0.01 /', S_n, '=', t, '.'
+             print 'The cumulative upper tail of the binomial distribution for', unique_n[n], 'trials, success probability 0.005, and >=', m, 'successes is', 1-scipy.stats.binom.cdf(m-1,int(unique_n[n]),0.005), '.'
+             print 'Thus, the minimum coverage of', label, 'sites required to potentially observe significant failed C->T conversion counts is: ' + str(unique_n[n])
+             break
+      if breakFlag == 1:
+         break
+
+
+infile = open(sys.argv[1], 'rb')
+fileCpG = open(sys.argv[2], 'w')
+fileCHG = open(sys.argv[3], 'w')
+fileCHH = open(sys.argv[4], 'w')
+
+fileCpG.write("SequenceID" + '\t' + "Position" + '\t' + "Strand" + '\t' + "Coverage" + '\t' + "Count_C" + '\t' + "Count_T" +'\n')
+fileCHG.write("SequenceID" + '\t' + "Position" + '\t' + "Strand" + '\t' + "Coverage" + '\t' + "Count_C" + '\t' + "Count_T" +'\n')
+fileCHH.write("SequenceID" + '\t' + "Position" + '\t' + "Strand" + '\t' + "Coverage" + '\t' + "Count_C" + '\t' + "Count_T" +'\n')
+
+coverageCpG = []
+coverageCHG = []
+coverageCHH = []
+
+with open(sys.argv[1], 'rb') as infile:
+  for line in infile:
+      output = ['N/A'] * 8;
+      input = (line.rstrip('\n')).split("\t");
+
+      coverage = int(input[3]) + int(input[4])
+      if coverage <> 0:
+          C_Freq = int(input[3])
+          T_Freq = int(input[4])
+      else:
+          C_Freq = 0
+          T_Freq = 0
+      altrow =input[0].split('|')
+      pattern=filter(None,altrow)
+      lengthpattern= len(pattern)-1
+      temp= (altrow[lengthpattern])
+      output[0]=temp
+      output[1]= input[1]
+      if input[2] == '+':
+          output[2] = "F"
+      else:
+          output[2] = "R"
+      output[3] = str(coverage)
+      output[4] = str(C_Freq)
+      output[5] = str(T_Freq)
+      output[6] = input[5]
+
+      if output[6] == 'CG':
+        fileCpG.writelines('\t'.join(output[0:6]) + '\n')
+        coverageCpG.append(coverage)
+      elif output[6] == 'CHG':
+        fileCHG.writelines('\t'.join(output[0:6]) + '\n')
+        coverageCHG.append(coverage)
+      elif output[6] == 'CHH':
+        fileCHH.writelines('\t'.join(output[0:6]) + '\n')
+        coverageCHH.append(coverage)
+      else:
+        print "\nUnknown context: neither CG, nor CHG, nor CHH.  How can that be?  Please check.\n"
+        exit()
+
+fileCpG.close()
+fileCHH.close()
+fileCHG.close()
+minNval(coverageCpG,'CpG')
+minNval(coverageCHG,'CHG')
+minNval(coverageCHH,'CHH')
---
+++ bin/filterMsam.pl
@@ -0,0 +1,373 @@
+#!/usr/bin/perl
+#filterMsam.pl
+# - BWASP Perl script to filter Msam records
+
+=begin comment
+
+Latest version: April 20, 2015	Author: Volker Brendel (vbrendel@indiana.edu)
+
+This script will take as input a combined alignment/methylation call file in SAM format,
+such as produced by Bismark, for either paired-end reads or single reads.  The script
+first determines the probabilities of Z, X, and H calls.  A second pass through the
+input determines read pairs (or single reads) that have a very small probability of
+being properly bisulfite-treated reads - in the extreme, reads that produce methylation
+calls in all possible C positions, independent of context.  The tacit assumption is that
+the most parsimonious explanation of the data is that these reads represent sequence
+evidence of DNA not properly exposed to the bisulfite-treatment.  Motivation for this
+script came from analysis of social insect methylomes that have invariably shown very
+low methylation rates (typically less than 1%).
+
+Threshold criteria are set in the code.  Our settings:
+	- reject a read (pair) with methylation call probability less than 0.01
+		(Bonferroni adjusted)
+	- with additional criterion that the count of non-methylation calls (z,x,h)
+		be less than 0.25 times the count of methylation calls (Z,X,H)
+
+Paired reads are rejected if either one of the reads fails the criterion (output files
+*.01fail and *.10fail) or both (output file *.11fail).
+
+=cut
+
+
+# Libraries and pragmas
+#use strict;
+use Getopt::Long;
+use Math::Pari qw(binomial gpui);
+
+# Set command line usage
+my $usage = "
+Usage: filterMsam.pl <options> --p|--s MsamFile
+
+  MsamFile               Mandotory Msam file
+  --p|--s            	 Specify either --p or --s for paired-end or single read SAM input
+  --outfile1=STRING	 Passed-filter Msam output file name (MsamFile.pass by default)
+  --outfile2=STRING	 Failed-filter Msam output file name (MsamFile.*fail by default)
+  --outpfile=STRING	 Summary output file name (MsamFile.summary by default)
+  --help                 Print this help message and exit
+
+";
+
+# Get options from the command line
+my $MsamFile    = "";
+my $paired      = "";
+my $single      = "";
+my $psoption    = "";
+my $outfile1    = "";
+my $outfile11   = "";
+my $outfile01   = "";
+my $outfile10   = "";
+my $outpfile    = "";
+my $help        = '';
+
+GetOptions(
+    "p"              => \$paired,
+    "s"              => \$single,
+    "outfile1=s"     => \$outfile1,
+    "outfile2=s"     => \$outfile11,
+    "outpfile=s"     => \$outpfile,
+    "help"           => \$help
+);
+
+if ($help || $paired eq $single) { printf( STDERR $usage ); exit; }
+
+my $MsamFile = shift(@ARGV)
+  or die("\n\nError: Please provide MsamFile file as input.\n$usage $!");
+if ( !-e $MsamFile ) {
+  die("\n\nError: Msam file $MsamFile does not exist.\n$usage $!");
+}
+if ($paired) {
+	$psoption = "--p";
+}
+else {
+	$psoption = "--s";
+}
+if ($paired) {
+  printf( STDERR "\nRunning filterMsam.pl with the following options:
+    MsamFile     = %s
+    --s|--p      = %s
+    outfile1     = %s
+    outfile2     = %s %s %s
+    outpfile     = %s\n\n", $MsamFile, $psoption, $outfile1, $outfile11, $outfile01, $outfile10, $outpfile
+  );
+}
+else {
+  printf( STDERR "\nRunning filterMsam.pl with the following options:
+    MsamFile     = %s
+    --s|--p      = %s
+    outfile1     = %s
+    outfile2     = %s
+    outpfile     = %s\n\n", $MsamFile, $psoption, $outfile1, $outfile11, $outpfile
+  );
+}
+
+
+if ($outfile1 eq "") {
+	$outfile1     = $MsamFile . ".pass";
+}
+else {
+	$outfile1     = $outfile1 . ".pass";
+}
+if ($outfile11 eq "") {
+	if ($paired) {
+		$outfile11     = $MsamFile . ".11fail";
+		$outfile01     = $MsamFile . ".01fail";
+		$outfile10     = $MsamFile . ".10fail";
+	}
+	else {
+		$outfile11     = $MsamFile . ".fail";
+	}
+}
+else {
+	if ($paired) {
+		$outfile01     = $outfile11 . ".01fail";
+		$outfile10     = $outfile11 . ".10fail";
+		$outfile11     = $outfile11 . ".11fail";
+	}
+	else {
+		$outfile11     = $outfile11 . ".fail";
+	}
+}
+if ($outpfile eq "") {
+	$outpfile     = $MsamFile . ".summary";
+}
+open( MSFILE,  "< $MsamFile" );
+open( MSPFILE, "> $outfile1" );
+open( MSF11FILE, "> $outfile11" );
+if ($paired) {
+  open( MSF01FILE, "> $outfile01" );
+  open( MSF10FILE, "> $outfile10" );
+}
+open( OUTFILE, "> $outpfile" );
+
+
+my ( $samstring, $mthstring, $samstring2 );
+my ( $zcount, $Zcount, $xcount, $Xcount, $hcount, $Hcount);
+my ( $zxhcount, $ZXHcount );
+my ( $Totalzcount, $TotalZcount, $Totalxcount, $TotalXcount, $Totalhcount, $TotalHcount) = (0, 0, 0, 0, 0, 0);
+
+my $scnt = 0;
+while ( $samstring = <MSFILE> ) {
+	if ($samstring =~ /^@[A-Za-z][A-Za-z](\t[A-Za-z][A-Za-z0-9]:[ -~]+)+$/ || $samstring =~ /^@CO\t.*/) {
+#         ... ignore SAM header lines, identified as per http://samtools.github.io/hts-specs/SAMv1.pdf
+	  next;
+	}
+	$scnt++;
+	if ($scnt%1000000 == 0) {
+	  printf STDERR "scnt= %12d\n", $scnt;
+	}
+        @a = split( "\t", $samstring );
+	$mthstring = substr($a[13],5);
+	$zcount = ($mthstring =~ tr/z//);
+	$Zcount = ($mthstring =~ tr/Z//);
+	$xcount = ($mthstring =~ tr/x//);
+	$Xcount = ($mthstring =~ tr/X//);
+	$hcount = ($mthstring =~ tr/h//);
+	$Hcount = ($mthstring =~ tr/H//);
+
+	$Totalzcount += $zcount;
+	$TotalZcount += $Zcount;
+	$Totalxcount += $xcount;
+	$TotalXcount += $Xcount;
+	$Totalhcount += $hcount;
+	$TotalHcount += $Hcount;
+}
+
+
+my ($pz, $pZ, $px, $pX, $ph, $pH);
+if ($Totalzcount+$TotalZcount > 0) {
+  $pz = $Totalzcount/($Totalzcount+$TotalZcount);
+  $pZ = 1.0 - $pz;
+}
+else {
+  $pz = 0.0;
+  $pZ = 0.0;
+}
+if ($Totalxcount+$TotalXcount > 0) {
+  $px = $Totalxcount/($Totalxcount+$TotalXcount);
+  $pX = 1.0 - $px;
+}
+else {
+  $px = 0.0;
+  $pX = 0.0;
+}
+if ($Totalhcount+$TotalHcount > 0) {
+  $ph = $Totalhcount/($Totalhcount+$TotalHcount);
+  $pH = 1.0 - $ph;
+}
+else {
+  $ph = 0.0;
+  $pH = 0.0;
+}
+
+
+printf OUTFILE "\nNumber of alignments: %12d\n", $scnt;
+printf OUTFILE "Prob(z)=%6.4f\tProb(Z)=%6.4f\t\tProb(x)=%6.4f\tProb(X)=%6.4f\t\tProb(h)=%6.4f\tProb(H)=%6.4f\n", $pz, $pZ, $px, $pX, $ph, $pH;
+
+close MSFILE;
+
+my $logS     = log(0.01/$scnt);
+my $fraction = 0.25;
+
+
+open( MSFILE, "< $MsamFile" );
+my $scnt = 0;
+my $pcnt = 0;
+my $f11cnt = 0;
+my $f01cnt = 0;
+my $f10cnt = 0;
+my %ZlogP = ();
+my %XlogP = ();
+my %HlogP = ();
+my $logP;
+my $reject_read1;
+
+while ( $samstring = <MSFILE> ) {
+	if ($samstring =~ /^@[A-Za-z][A-Za-z](\t[A-Za-z][A-Za-z0-9]:[ -~]+)+$/ || $samstring =~ /^@CO\t.*/) {
+#         ... ignore SAM header lines, identified as per http://samtools.github.io/hts-specs/SAMv1.pdf
+	  next;
+	}
+	$scnt++;
+        @a = split( "\t", $samstring );
+	$mthstring = substr($a[13],5);
+
+	$logP = read_calls_probability($mthstring);
+
+	if ($paired == 0) {
+		if ($logP < $logS  &&  $zxhcount < $fraction * $ZXHcount) {
+			$f11cnt++;
+			print MSF11FILE "$samstring";
+		}
+		else {
+			$pcnt++;
+			print MSPFILE "$samstring";
+		}
+	}
+	else {
+		if ($logP < $logS  &&  $zxhcount < $fraction * $ZXHcount) {
+			$reject_read1 = 1;
+		}
+		else {
+			$reject_read1 = 0;
+		}
+		$samstring2 = <MSFILE>;
+        	@a = split( "\t", $samstring2 );
+		$mthstring = substr($a[13],5);
+
+		$logP = read_calls_probability($mthstring);
+
+		if ($logP < $logS  &&  $zxhcount < $fraction * $ZXHcount) {
+			if ($reject_read1) {
+				$f11cnt++;
+				print MSF11FILE "$samstring";
+				print MSF11FILE "$samstring2";
+			}
+			else {
+				$f01cnt++;
+				print MSF01FILE "$samstring";
+				print MSF01FILE "$samstring2";
+			}
+		}
+		else {
+			if ($reject_read1) {
+				$f10cnt++;
+				print MSF10FILE "$samstring";
+				print MSF10FILE "$samstring2";
+			}
+			else {
+				$pcnt++;
+				print MSPFILE "$samstring";
+				print MSPFILE "$samstring2";
+			}
+		}
+	}
+
+	if ($scnt%1000000 == 0) {
+	  if ($paired == 0) {
+	    printf STDERR "scnt= %12d: pcnt= %12d, fcnt= %12d\n", $scnt, $pcnt, $f11cnt;
+	  }
+	  else {
+	    printf STDERR "scnt= %12d: pcnt= %12d, fcnt11= %12d, fcnt01= %12d, fcnt10= %12d\n", $scnt, $pcnt, $f11cnt, $f01cnt, $f10cnt;
+	  }
+	}
+}
+printf OUTFILE "\nThreshold:	logS=%12.2f\n", $logS;
+printf OUTFILE "\nscnt=\t%12d", $scnt;
+printf OUTFILE "\npcnt=\t%12d", $pcnt;
+if ($paired == 0) {
+  printf OUTFILE "\nfcnt=\t%12d\t(%6.2f%%)\n", $f11cnt, 100.0*$f11cnt/$scnt;
+}
+else {
+  printf OUTFILE "\nf11cnt=\t%12d\t(%6.2f%%)\n", $f11cnt, 100.0*$f11cnt/$scnt;
+  printf OUTFILE   "f01cnt=\t%12d\t(%6.2f%%)\n", $f01cnt, 100.0*$f01cnt/$scnt;
+  printf OUTFILE   "f10cnt=\t%12d\t(%6.2f%%)\n", $f10cnt, 100.0*$f10cnt/$scnt;
+}
+
+
+close MSFILE;
+close MSF11FILE;
+if ($paired) {
+  close MSF01FILE;
+  close MSF10FILE;
+}
+close OUTFILE;
+
+
+
+sub read_calls_probability {
+  my ($mthstring) = @_;
+
+  $zcount = ($mthstring =~ tr/z//);
+  $Zcount = ($mthstring =~ tr/Z//);
+  $xcount = ($mthstring =~ tr/x//);
+  $Xcount = ($mthstring =~ tr/X//);
+  $hcount = ($mthstring =~ tr/h//);
+  $Hcount = ($mthstring =~ tr/H//);
+
+  $zxhcount = $zcount + $xcount + $hcount;
+  $ZXHcount = $Zcount + $Xcount + $Hcount;
+
+  if (! exists $ZlogP{"$Zcount,$zcount"} ) {
+    $ZlogP{"$Zcount,$zcount"} = log_binomial_tail($Zcount,$Zcount+$zcount,$pZ);
+  }
+  if (! exists $XlogP{"$Xcount,$xcount"} ) {
+    $XlogP{"$Xcount,$xcount"} = log_binomial_tail($Xcount,$Xcount+$xcount,$pX);
+  }
+  if (! exists $HlogP{"$Hcount,$hcount"} ) {
+    $HlogP{"$Hcount,$hcount"} = log_binomial_tail($Hcount,$Hcount+$hcount,$pH);
+  }
+
+  $logP = $ZlogP{"$Zcount,$zcount"} + $XlogP{"$Xcount,$xcount"} + $HlogP{"$Hcount,$hcount"};
+
+  return ($logP);
+}
+
+
+
+sub log_binomial_tail {
+   my ($k, $n, $p) = @_;
+   my $x;
+   my $TP = 0.0;
+   my $MINTP = 1e-20;
+
+  if ($n == 0) {
+    return 0.0;
+  }
+  if ($p == 0.0) {
+    return 0.0;
+  }
+
+  if ($k > $n/2) {
+    for ($x = $k; $x <= $n; $x++) {
+      $TP += (binomial($n,$x) * gpui($p,$x) * gpui(1.0 - $p, $n - $x));
+    }
+    if ($TP < $MINTP) {$TP = $MINTP;}
+    return log($TP);
+  }
+  else {
+    for ($x = $k-1; $x >= 0; $x--) {
+      $TP += (binomial($n,$x) * gpui($p,$x) * gpui(1.0 - $p, $n - $x));
+    }
+    if ($TP > 1.0 - $MINTP) {$TP = 1.0 - $MINTP;}
+    return log(1.0-$TP);
+  }
+}
