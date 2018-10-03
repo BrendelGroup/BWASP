57dbb5a603b7ce5184e5ae7fbd04d3ba3a3a2121
Volker Brendel <vbrendel@indiana.edu>
2018-10-02 21:16:02 -0400

    calculating median raw read length in sstats*.sh

--- bin/sstats-se.sh
+++ bin/sstats-se.sh
@@ -23,12 +23,14 @@ RAWREAD_NB1=`wc -l ${SAMPLE}.fastq|cut -d' ' -f1`
 RAWREAD_NB=`awk "BEGIN {printf \"%.0f\", ${RAWREAD_NB1}/4}"`
 echo "Number of raw reads: ${RAWREAD_NB}" > ${SAMPLE}.stats
 
-RAWREAD_LGTH=`awk 'NR==9' FastQC/${SAMPLE}_fastqc/fastqc_data.txt | awk -F" " '{printf "%.0f", $3}'`
-echo "Raw read length (per FastQC report): ${RAWREAD_LGTH}" >> ${SAMPLE}.stats
+RAWREAD_LGTH=`awk 'NR==9' FastQC/${SAMPLE}_1_fastqc/fastqc_data.txt | awk -F" " '{print $3}'`
+echo "Raw read length range (per FastQC report): ${RAWREAD_LGTH}" >> ${SAMPLE}.stats
+
+RAWREAD_ML=`cat ${SAMPLE}_1.fastq | awk '{if(NR%4==2) printf "%.0f\n", length($1)}' > /tmp/${SAMPLE}_readlength1.txt; sort -n /tmp/${SAMPLE}_readlength1.txt | awk ' { a[i++]=$1; } END { printf "%.0f", a[int(i/2)]; }'`
+echo "Median length of raw reads: ${RAWREAD_ML1}" >> ${SAMPLE}.stats
 
 RAWSAMPLE_SZE=`awk 'BEGIN{sum=0;}{if(NR%4==2){sum+=length($0);}}END{printf "%.0f", sum;}' ${SAMPLE}.fastq`
 echo "Raw read sample size: ${RAWSAMPLE_SZE} bp" >> ${SAMPLE}.stats
-
 
 TRMREAD_NB1=`wc -l ${SAMPLE}_trimmed.fq|cut -d' ' -f1`
 TRMREAD_NB=`awk "BEGIN {printf \"%.0f\", ${TRMREAD_NB1}/4}"`
