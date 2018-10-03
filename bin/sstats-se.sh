b8a3e07356ce5abd9464e679bc28f95a0f113239
Volker Brendel <vbrendel@indiana.edu>
2018-10-03 12:08:12 -0400

    added median read length determination to sstats-*.sh

--- bin/sstats-se.sh
+++ bin/sstats-se.sh
@@ -23,11 +23,11 @@ RAWREAD_NB1=`wc -l ${SAMPLE}.fastq|cut -d' ' -f1`
 RAWREAD_NB=`awk "BEGIN {printf \"%.0f\", ${RAWREAD_NB1}/4}"`
 echo "Number of raw reads: ${RAWREAD_NB}" > ${SAMPLE}.stats
 
-RAWREAD_LGTH=`awk 'NR==9' FastQC/${SAMPLE}_1_fastqc/fastqc_data.txt | awk -F" " '{print $3}'`
+RAWREAD_LGTH=`awk 'NR==9' FastQC/${SAMPLE}_fastqc/fastqc_data.txt | awk -F" " '{print $3}'`
 echo "Raw read length range (per FastQC report): ${RAWREAD_LGTH}" >> ${SAMPLE}.stats
 
-RAWREAD_ML=`cat ${SAMPLE}_1.fastq | awk '{if(NR%4==2) printf "%.0f\n", length($1)}' > /tmp/${SAMPLE}_readlength1.txt; sort -n /tmp/${SAMPLE}_readlength1.txt | awk ' { a[i++]=$1; } END { printf "%.0f", a[int(i/2)]; }'`
-echo "Median length of raw reads: ${RAWREAD_ML1}" >> ${SAMPLE}.stats
+RAWREAD_ML=`cat ${SAMPLE}.fastq | awk '{if(NR%4==2) printf "%.0f\n", length($1)}' > /tmp/${SAMPLE}_readlength.txt; sort -n /tmp/${SAMPLE}_readlength.txt | awk ' { a[i++]=$1; } END { printf "%.0f", a[int(i/2)]; }'`
+echo "Median length of raw reads: ${RAWREAD_ML}" >> ${SAMPLE}.stats
 
 RAWSAMPLE_SZE=`awk 'BEGIN{sum=0;}{if(NR%4==2){sum+=length($0);}}END{printf "%.0f", sum;}' ${SAMPLE}.fastq`
 echo "Raw read sample size: ${RAWSAMPLE_SZE} bp" >> ${SAMPLE}.stats
@@ -38,7 +38,7 @@ echo "Number of trimmed reads: ${TRMREAD_NB}" >> ${SAMPLE}.stats
 
 TRMREAD_LGTH=`awk 'NR==9' FastQC/${SAMPLE}_trimmed_fastqc/fastqc_data.txt | awk -F" " '{print $3}'`
 echo "Trimmed read length range (per FastQC report): ${TRMREAD_LGTH}" >> ${SAMPLE}.stats
-TRMREAD_ML=`cat ${SAMPLE}_trimmed.fq | awk '{if(NR%4==2) printf "%.0f\n", length($1)}' > /tmp/${SAMPLE}_readlength1.txt; sort -n /tmp/${SAMPLE}_readlength1.txt| awk ' { a[i++]=$1; } END { printf "%0.f", a[int(i/2)]; }'`
+TRMREAD_ML=`cat ${SAMPLE}_trimmed.fq | awk '{if(NR%4==2) printf "%.0f\n", length($1)}' > /tmp/${SAMPLE}_readlength.txt; sort -n /tmp/${SAMPLE}_readlength.txt| awk ' { a[i++]=$1; } END { printf "%0.f", a[int(i/2)]; }'`
 echo "Median length of trimmed reads: ${TRMREAD_ML}" >> ${SAMPLE}.stats
 
 TRMSAMPLE_SZE=`awk 'BEGIN{sum=0;}{if(NR%4==2){sum+=length($0);}}END{printf "%.0f", sum;}' ${SAMPLE}_trimmed.fq`
